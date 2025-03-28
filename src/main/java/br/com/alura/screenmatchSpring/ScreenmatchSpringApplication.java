f65c73f5f87e2422ca7e7deedb8bfd7193c7cebe
raidejunior <raidejunior@gmail.com>
2025-03-27 23:03:55 -0300

    first commit

---
+++ src/main/java/br/com/alura/screenmatchSpring/ScreenmatchSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatchSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchSpringApplication.class, args);
+	}
+
+}
