42c0eb087b47875d96bcbe6f492eef8877a9cef2
raidejunior <raidejunior@gmail.com>
2025-04-07 11:59:14 -0300

    update

--- src/main/java/br/com/alura/screenmatchSpring/repository/SerieRepository.java
+++ src/main/java/br/com/alura/screenmatchSpring/repository/SerieRepository.java
@@ -19,6 +19,7 @@ public interface SerieRepository extends JpaRepository<Serie, Long> {
     List<Serie> findByGenero(Categoria categoria);
 
     List<Serie> findByTotalTemporadasLessThanEqualAndAvaliacaoGreaterThanEqual(int totalTemporadas, double avaliacao);
+
     @Query("select s from Serie s WHERE s.totalTemporadas <= :totalTemporadas AND s.avaliacao >= :avaliacao")
     List<Serie> seriesPorTemporadaEAValiacao(int totalTemporadas, double avaliacao);
     @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE e.titulo ILIKE %:trechoEpisodio%")
